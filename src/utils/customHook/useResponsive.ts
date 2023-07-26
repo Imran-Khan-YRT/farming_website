700bc91d39f5fbabf0ddf88a831c7ea30b0f734e
imrankhan <imran.khan@marginedge.com>
2023-07-26 15:39:13 +0600

    add responsive custom hook

---
+++ src/utils/customHook/useResponsive.ts
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from "react";
+
+interface Responsive {
+  isMobile: boolean;
+  isTablet: boolean;
+  isPC: boolean;
+}
+
+const useResponsive = (): Responsive => {
+  const [responsive, setResponsive] = useState<Responsive>({
+    isMobile: false,
+    isTablet: false,
+    isPC: true, // By default, consider it as PC view
+  });
+
+  const handleResize = () => {
+    const width = window.innerWidth;
+    setResponsive({
+      isMobile: width <= 550,
+      isTablet: width > 550 && width <= 750,
+      isPC: width > 750,
+    });
+  };
+
+  useEffect(() => {
+    handleResize(); // Set initial value on mount
+
+    // Attach event listener for resizing
+    window.addEventListener("resize", handleResize);
+
+    // Clean up the event listener on unmount
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return responsive;
+};
+
+export default useResponsive;
