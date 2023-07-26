6b75bea846bea45526932e185f2e5fe5a289913c
imrankhan <imran.khan@marginedge.com>
2023-07-26 15:40:55 +0600

    add routes in index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -3,15 +3,19 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { ThemeProvider } from "./utils/themeContext/ThemeContext";
-
+import GetRoutes from "./utils/Route";
+import { BrowserRouter } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <React.StrictMode>
-    {/* Suspense to load the translation file */}
-    <Suspense fallback="Loading...">
-      <ThemeProvider>
-        <App />
-      </ThemeProvider>
-    </Suspense>
+    <BrowserRouter>
+      {/* Suspense to load the translation file */}
+      <Suspense fallback="Loading...">
+        <ThemeProvider>
+          <App />
+          <GetRoutes />
+        </ThemeProvider>
+      </Suspense>
+    </BrowserRouter>
   </React.StrictMode>
 );
